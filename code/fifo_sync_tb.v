04225063984d3a8c42315d5cce7a608d41c1fd9c
K Subhamraj Patra <subhamrajpatra83@gmail.com>
2025-07-25 15:40:26 +0530

    Update fifo_sync_tb.v

--- code/fifo_sync_tb.v
+++ code/fifo_sync_tb.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/09/2025 07:03:51 PM
-// Design Name: 
-// Module Name: fifo_sync_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module fifo_sync_tb;
 
     // Parameters
